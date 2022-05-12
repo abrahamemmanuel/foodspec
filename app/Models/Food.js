92a5c2017ea18e26fbf4041ff16debc0a6af14fe
Abraham Emmanuel <sundayemmanuelabraham@gmail.com>
2022-05-12 12:29:15 +0100

    added model

---
+++ app/Models/Food.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const FoodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+    min: [0, 'Price must be greater than 0']
+  },
+
+  specifications: [
+    {
+      type: String,
+      required: [true, 'Specifications are required']
+    }
+  ],
+
+  createdAt: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = mongoose.model('Food', FoodSchema);
